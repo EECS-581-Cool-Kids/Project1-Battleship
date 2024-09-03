a688a6743000ff48e24f2246e47c9127aebaed27
Jacob Wilkus <jwilkus1@gmail.com>
2024-09-03 11:57:32 -0500

    Refactor

--- Battleship/BattleshipGame.cs
+++ Battleship/BattleshipGame.cs
@@ -55,29 +55,7 @@ namespace Battleship
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // Load the grid textures
-            GridTile[,] gridArray = _grid.GridArray;
-
-            gridArray[0, 0].GridTexture = Content.Load<Texture2D>("top_corner");
-
-            for (int tileNum = 1; tileNum < _grid.Size; tileNum++)
-            {
-                gridArray[0, tileNum].GridTexture = Content.Load<Texture2D>($"column_{tileNum}");
-                gridArray[tileNum, 0].GridTexture = Content.Load<Texture2D>($"row_{tileNum}");
-            }
-
-            for (int colNum = 1; colNum <  _grid.Size; colNum++)
-            {
-                for (int rowNum = 1; rowNum < _grid.Size; rowNum++)
-                {
-                    gridArray[colNum, rowNum].GridTexture = Content.Load<Texture2D>("square");
-                    gridArray[colNum, rowNum].CanSelect = true;
-                }
-            }
-
-            _grid.SquareSelectedTexture = Content.Load<Texture2D>("square_selected");
-            _grid.SquareMissedTexture = Content.Load<Texture2D>("square_miss");
-            _grid.SquareHitTexture = Content.Load<Texture2D>("square_hit");
+            _grid.LoadContent(Content);
         }
 
         /// <summary>
@@ -89,25 +67,7 @@ namespace Battleship
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // Get which square the mouse is inside of
-            MouseState mouseState = Mouse.GetState();
-            Point moustPoint = new Point(mouseState.X, mouseState.Y);
-            foreach (GridTile tile in _grid.GridArray)
-            {
-                if (tile.GridRectangle.Contains(moustPoint) && tile.CanSelect)
-                {
-                    tile.MouseOver = true;
-
-                    if (mouseState.LeftButton == ButtonState.Pressed && !tile.IsMiss && !tile.IsHit)
-                        tile.IsMiss = true;
-                    if (mouseState.RightButton == ButtonState.Pressed && !tile.IsMiss && !tile.IsHit)
-                        tile.IsHit = true;
-                }
-                else if (!tile.GridRectangle.Contains(moustPoint) && tile.MouseOver)
-                {
-                    tile.MouseOver = false;
-                }
-            }
+            _grid.Update();
 
             base.Update(gameTime);
         }
@@ -121,24 +81,7 @@ namespace Battleship
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
-
-            // Draw the grid
-            GridTile[,] gridArray = _grid.GridArray;
-            foreach (GridTile tile in gridArray)
-            {
-                Texture2D texture;
-                if (tile.IsMiss)
-                    texture = _grid.SquareMissedTexture;
-                else if (tile.IsHit)
-                    texture = _grid.SquareHitTexture;
-                else if (tile.MouseOver)
-                    texture = _grid.SquareSelectedTexture;
-                else
-                    texture = tile.GridTexture;
-
-                _spriteBatch.Draw(texture, tile.GridRectangle, Color.White);
-            }
-
+            _grid.Draw(_spriteBatch);
             _spriteBatch.End();
 
             base.Draw(gameTime);
