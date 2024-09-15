aa7edf89aa82ac3049437488d1555bb0456a2fc9
modmo22 <modcox2@gmail.com>
2024-09-14 23:20:15 -0500

    Finalized Ship.cs comments

--- Battleship/Ship.cs
+++ Battleship/Ship.cs
@@ -1,4 +1,15 @@
-﻿using Microsoft.Xna.Framework;
+﻿/*
+ *   Module Name: Ship.cs
+ *   Purpose: This module is the Ship class for the Battleship game. It represents a ship.
+ *   Inputs: None
+ *   Output: None
+ *   Additional code sources:
+ *   Developers: Derek Norton, Ethan Berkley, Jacob Wilkus, Mo Morgan, and Richard Moser
+ *   Date: 09/11/2024
+ *   Last Modified: 09/14/2024
+ */
+
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
@@ -53,6 +64,9 @@ namespace Battleship
         /// </summary>
         public List<GridTile> ShipTiles { get; set; } = new();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Ship"/> class.
+        /// </summary>
         public Ship(Point position, Point size, int length)
         {
             ShipRectangle = new Rectangle(position, size);
