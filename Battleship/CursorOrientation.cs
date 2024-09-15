bb85428a5964c0af2980195354ff17df087c0ada
modmo22 <modcox2@gmail.com>
2024-09-14 23:53:42 -0500

    Finalized CursorOrientation.cs comments

--- Battleship/CursorOrientation.cs
+++ Battleship/CursorOrientation.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿/*
+ *   Module Name: CursorOrientation.cs
+ *   Purpose: This module is the cursor orientation enum for the Battleship game. 
+ *            It is used to determine the orientation of the cursor on the game board.
+ *   Inputs: None
+ *   Output: None
+ *   Additional code sources:
+ *   Developers: Derek Norton, Ethan Berkley, Jacob Wilkus, Mo Morgan, and Richard Moser
+ *   Date: 09/11/2024
+ *   Last Modified: 09/14/2024
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
