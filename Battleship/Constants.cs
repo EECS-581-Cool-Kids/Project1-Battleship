6932cfc1496efa5f7708d58fa7f40f8921d79eef
modmo22 <modcox2@gmail.com>
2024-09-15 22:50:16 -0500

    Tiny edits to Constants.cs

--- Battleship/Constants.cs
+++ Battleship/Constants.cs
@@ -32,12 +32,12 @@ namespace Battleship
         /// </summary>
         public const int GRID_SIZE = 11;
 
-        ///<summary>
+        /// <summary>
         /// Player 1 grid offset value.
         /// </summary>
         public const int PLAYER_1_OFFSET = 0;
 
-        ///<summary>
+        /// <summary>
         /// Player 2 grid offset value.
         /// </summary>
         public const int PLAYER_2_OFFSET = SQUARE_SIZE * GRID_SIZE * SCALE; // should be SQUARE_SIZE * GRID_SIZE * 2 * SCALE, i.e. 16 * 11 * 2 * scale / 2
